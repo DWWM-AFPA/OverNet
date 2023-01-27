86bad5d45b5f5cd892ffd3279fd0ae5f76d5010c
Florian <debiasif31@gmail.com>
2023-01-27 12:04:47 +0100

    Je ne sais pas trop ce qui a été fait mais ca a été fait !

--- OverNet/src/Main.java
+++ OverNet/src/Main.java
@@ -1,5 +1,11 @@
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        __ProxyServer test = new __ProxyServer();
+
+            test.run("http://www.gobland.fr");
     }
 }
--- OverNet/src/__ProxyServer.java
+++ OverNet/src/__ProxyServer.java
@@ -35,30 +35,120 @@ public class __ProxyServer extends Thread {
         return socket;
     }
 
-    public static HttpResponse monProxy(HttpRequest myRequest, InetAddress addr, int port) throws IllegalArgumentException{
 
-            InetSocketAddress socket = new InetSocketAddress(addr, port);
-            ProxySelector myProxyAdress = ProxySelector.of(socket);
+    public static HttpRequest httpRequestFromString(String monUrl){
+        HttpRequest retour = null;
+        try {
+            retour =  HttpRequest.newBuilder()
+                    .uri(URI.create(monUrl))
+                    .build();
+        }
+        catch (IllegalStateException e){
+            System.err.println("httpRequestFromString ne fonctionne pas :" + e);
+        }
+        catch (Exception e){
+            System.err.println("CA MARCHE PAS : " + e);
+        }
+        System.out.println(retour);
+        return retour;
+    }
+
+    public static HttpResponse monProxy(String myRequest, InetAddress addr, int port){
+
+        HttpRequest request = httpRequestFromString(myRequest);
+        InetSocketAddress socket = new InetSocketAddress(addr, port);
+        ProxySelector myProxyAdress = ProxySelector.of(socket);
+        try{
+            return HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(20))
+                    .proxy(myProxyAdress)
+                    .authenticator(Authenticator.getDefault())
+                    .build().send(request, HttpResponse.BodyHandlers.ofInputStream());
+        }
+        catch (IOException e){
+            System.err.println("IOException :" + e);
+            return null;
+        }
+        catch (InterruptedException e){
+            System.err.println("InterruptedException :" + e);
+            return null;
+        }
+        catch (IllegalArgumentException e){
+            System.err.println("L'argument de mon socket est pas bon :" + e);
+            return null;
+        }
+        catch (Exception e){
+            System.err.println("CA MARCHE PAS : " + e);
+            return null;
+        }
+    }
+
+    public static HttpResponse monProxy(String myRequest, String addr, int port){
+
+        HttpRequest request = httpRequestFromString(myRequest);
+        InetSocketAddress socket = new InetSocketAddress(addr, port);
+        ProxySelector myProxyAdress = ProxySelector.of(socket);
+        try{
             HttpClient client = HttpClient.newBuilder()
                     .connectTimeout(Duration.ofSeconds(20))
                     .proxy(myProxyAdress)
                     .authenticator(Authenticator.getDefault())
                     .build();
-            HttpResponse response = client.send(myRequest, HttpResponse.BodyHandlers.ofInputStream());
+            return client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+        }
+            catch (IOException e){
+            System.err.println("IOException :" + e);
+            return null;
+        }
+            catch (InterruptedException e){
+            System.err.println("InterruptedException :" + e);
+            return null;
+        }
+            catch (IllegalArgumentException e){
+            System.err.println("L'argument de mon socket est pas bon :" + e);
+            return null;
+        }
+            catch (Exception e){
+            System.err.println("CA MARCHE PAS : " + e);
+            return null;
+        }
+    }
 
-            return response;
+    public static HttpResponse monProxy(String myRequest){
+
+        HttpRequest request = httpRequestFromString(myRequest);
+        try {
+            HttpClient client = HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(20))
+                    .authenticator(Authenticator.getDefault())
+                    .build();
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+            catch (IOException e){
+            System.err.println("IOException :" + e);
+            return null;
+        }
+            catch (InterruptedException e){
+            System.err.println("InterruptedException :" + e);
+            return null;
+        }
+            catch (IllegalArgumentException e){
+            System.err.println("L'argument de mon socket est pas bon :" + e);
+            return null;
+        }
+            catch (Exception e){
+            System.err.println("CA MARCHE PAS : " + e);
+            System.out.println("request " + request);
+            System.out.println("pasrequest " + HttpResponse.BodyHandlers.ofString());
+            return null;
+        }
 
     }
 
-    public static HttpResponse monProxy(HttpRequest myRequest) throws IllegalArgumentException{
+    public void run(String myRequest){
+        __ProxyServer test = new __ProxyServer();
 
-        HttpClient client = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(20))
-                .authenticator(Authenticator.getDefault())
-                .build();
-        HttpResponse response = client.send(myRequest, HttpResponse.BodyHandlers.ofInputStream());
-
-        return response;
+    System.out.println(test.monProxy(myRequest));
 
     }
 }
