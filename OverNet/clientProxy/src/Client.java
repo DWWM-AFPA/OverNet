44617dec9e31603cc973269a1c732e7b182f4dee
Florian <debiasif31@gmail.com>
2023-02-20 11:04:55 +0100

    ajout des usecase, diag sequence, et diag d'etat

--- OverNet/clientProxy/src/Client.java
+++ OverNet/clientProxy/src/Client.java
@@ -6,9 +6,9 @@ public class Client {
         __ProxyServS testS = new __ProxyServS();
         //test2.test("http://www.gobland.fr", "127.0.0.1", 5000);
         //test2.test("https://www.gobland.fr", "127.0.0.1", 5000);
-        //test2.test("www.gobland.fr", "127.0.0.1", 5000);
+        test2.test("www.gobland.fr", "127.0.0.1", 5000);
         //test2.testhtml("www.google.fr", "127.0.0.1", 5000);
-        testS.testhtml("https://www.google.fr");
+        //testS.testhtml("https://www.google.fr");
         //test2.test("http://www.gobland.fr");
 
     }
